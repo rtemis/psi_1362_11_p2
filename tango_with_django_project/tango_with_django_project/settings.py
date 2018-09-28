61264353b42672a0b302907b26e6c73caf4ea13b
rtemis <leah_hadeed@yahoo.com>
2018-09-28 23:48:00 +0200

    Hecho la semana 1

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -54,6 +54,15 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [STATIC_DIR, ]
+
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -65,6 +74,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+				'django.template.context_processors.media',
             ],
         },
     },
