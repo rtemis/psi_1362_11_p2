af971c98f7f07db2d1c175724918d5bfb9ea3071
rtemis <leah_hadeed@yahoo.com>
2018-10-04 21:47:46 +0200

    empezado los ejercicios

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.contrib import admin
+from rango.models import Category, Page
 
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Page)
