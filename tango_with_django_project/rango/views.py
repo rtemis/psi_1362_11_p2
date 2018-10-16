c99a83e94e9a3d33ac3579f0587bff0105606229
Tu Nombre <you@example.com>
2018-10-16 17:24:01 +0200

    hasta heroku

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -23,7 +23,7 @@ def index(request):
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    return render(request, 'rango/about.html')
+    return render(request, 'rango/about.html', {})
 
 def show_category(request, category_name_slug):
 	# Create a context dictionary which we can pass
