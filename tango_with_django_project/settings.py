be00ffee9fc841c990744ac7eacbe784f8a520fa
Tu Nombre <you@example.com>
2018-10-19 19:35:05 +0200

    Variable ALLOWED_HOST modificada

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'yiy%odjmglg6t8shk*7&3wiaan^obl)w3)%6q6ck_877!%b+t0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [u'morning-dawn-44077.herokuapp.com']
 
 # Application definition
 
